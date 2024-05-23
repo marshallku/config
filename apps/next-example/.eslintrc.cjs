76c4bf7e73c25a0d00c8f13490a6045a705822bf
Marshall Ku <marshall@kakao.com>
2024-05-23 15:15:12 +0900

    Remove nextjs config

--- apps/next-example/.eslintrc.cjs
+++ apps/next-example/.eslintrc.cjs
@@ -3,7 +3,7 @@ require("@marshallku/eslint-config/patch");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
     env: { browser: true, es2020: true },
-    extends: ["@marshallku/eslint-config", "@marshallku/eslint-config/mixins/next"],
+    extends: ["@marshallku/eslint-config", "@marshallku/eslint-config/mixins/react"],
     settings: {
         react: {
             version: "18.2",

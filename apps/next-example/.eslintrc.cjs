d7766093c5005c229a32028b1068ead2364e56ea
Marshall Ku <marshall@kakao.com>
2024-05-23 14:59:56 +0900

    Add nextjs example app

---
+++ apps/next-example/.eslintrc.cjs
@@ -0,0 +1,17 @@
+require("@marshallku/eslint-config/patch");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    env: { browser: true, es2020: true },
+    extends: ["@marshallku/eslint-config", "@marshallku/eslint-config/mixins/next"],
+    settings: {
+        react: {
+            version: "18.2",
+        },
+    },
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+    },
+};
